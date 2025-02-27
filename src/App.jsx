caf7606c9b09305e9db6887a8b41b3d7d2399e71
Udeme98 <Darealudeme1@gmail.com>
2025-02-27 09:58:40 +0100

    introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-function App() {
-  return (
-    <>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was created Jordan Walke</li>
-        <li>Has well over 100k stars in Github </li>
-        <li>Is maintain by Facebook</li>
-      </ul>
-    </>
-  );
-}
+import Intro from "./Intro/Intro";
 
+const App = () => {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+};
 export default App;
---
+++ src/Intro/Intro.jsx
@@ -0,0 +1,15 @@
+const Intro = () => {
+  return (
+    <div>
+      <img src="/src/assets/react.svg" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was created Jordan Walke</li>
+        <li>Has well over 100k stars in Github </li>
+        <li>Is maintain by Facebook</li>
+      </ul>
+    </div>
+  );
+};
+export default Intro;
