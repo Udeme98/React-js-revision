3be39758ae428cf9d15a65e5749fb8c28919ae67
Udeme Udeme <darealudeme1@gmail.com>
2025-03-07 05:46:25 +0100

    meme generator - ternary operator

--- src/Meme generator/Count.jsx
+++ src/Meme generator/Count.jsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 
 const Count = () => {
   const [count, setCount] = useState(0);
+  const [isGoingOut, setIsGoingOut] = useState(true);
 
   const add = function () {
-    console.log("add");
+    setCount((prev) => prev + 1);
   };
   const sub = function () {
-    console.log("sub");
+    setCount((prev) => prev - 1);
   };
 
+  const toggle = () => {
+    setIsGoingOut((prev) => !prev);
+  };
   return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      <button onClick={sub}>-</button>
-      <h5>{count}</h5>
-      <button onClick={add}>+</button>
-    </div>
+    <>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <button onClick={sub}>-</button>
+        <h5>{count}</h5>
+        <button onClick={add}>+</button>
+      </div>
+
+      <h2 onClick={toggle}>{isGoingOut ? "Yes" : "No"}</h2>
+    </>
   );
 };
 
